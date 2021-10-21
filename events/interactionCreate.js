03b9e31eb7913e6fb55d18306930251dd6e75965
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-21 19:29:05 +0200

    ✨ Manage channels

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,25 @@
-module.exports = (client, int) => {
+module.exports = async (client, int) => {
     if (!int.isButton()) return;
 
     switch (int.customId) {
         case 'newTicket': {
-            return int.reply({ content: `Your ticket is open <@${int.member.id}> ✅`, ephemeral: true });
+            const channel = int.guild.channels.cache.find(x => x.name === `ticket-${int.member.id}`);
+
+            if (!channel) {
+                await int.guild.channels.create(`ticket-${int.member.id}`, {
+                    type: 'GUILD_TEXT',
+                    permissionOverwrites: [{
+                        id: int.guild.id,
+                        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+                    }]
+                });
+
+                const channel = int.guild.channels.cache.find(x => x.name === `ticket-${int.member.id}`);
+
+                return int.reply({ content: `Your ticket is open <@${int.member.id}> <#${channel.id}> ✅`, ephemeral: true });
+            } else {
+                return int.reply({ content: `You already have an open ticket <#${channel.id}> ❌`, ephemeral: true });
+            }
         }
     }
 };
