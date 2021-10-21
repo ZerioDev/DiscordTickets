b5528ec5fac8d3f5f33f41254e1f078a25d7f34c
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-21 18:51:33 +0200

    ⚡️ Commands

--- src/loader.js
+++ src/loader.js
@@ -16,13 +16,11 @@ for (const file of events) {
 
 console.log(`Loading commands...`);
 
-readdirSync('./commands/').forEach(dirs => {
-    const commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+const commands = readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
-    for (const file of commands) {
-        const command = require(`../commands/${dirs}/${file}`);
-        console.log(`-> Loaded command ${command.name.toLowerCase()}`);
-        client.commands.set(command.name.toLowerCase(), command);
-        delete require.cache[require.resolve(`../commands/${dirs}/${file}`)];
-    };
-});
+for (const file of commands) {
+    const command = require(`../commands/${file}`);
+    console.log(`-> Loaded command ${command.name.toLowerCase()}`);
+    client.commands.set(command.name.toLowerCase(), command);
+    delete require.cache[require.resolve(`../commands/${file}`)];
+};
