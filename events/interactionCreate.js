ec65382d2254283b2b5253b999b8603582bd1281
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-21 19:39:35 +0200

    ✨ Creation message

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,5 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+
 module.exports = async (client, int) => {
     if (!int.isButton()) return;
 
@@ -16,11 +18,31 @@ module.exports = async (client, int) => {
                         {
                             id: int.member.id,
                             allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
+                        },
+                        {
+                            id: client.user.id,
+                            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
                         }
                     ]
                 });
 
                 const channel = int.guild.channels.cache.find(x => x.name === `ticket-${int.member.id}`);
+
+                const ticketEmbed = new MessageEmbed();
+
+                ticketEmbed.setColor('GREEN');
+                ticketEmbed.setAuthor(`Your ticket has been successfully created ${int.member.user.username} ✅`);
+                ticketEmbed.setDescription('*To close the current ticket click on the reaction below, warning it is impossible to go back !*');
+
+                const closeButton = new MessageButton();
+
+                closeButton.setStyle('DANGER');
+                closeButton.setLabel('Close this ticket');
+                closeButton.setCustomId('closeTicket');
+
+                const row = new MessageActionRow().addComponents(closeButton);
+
+                await channel.send({ embeds: [ticketEmbed], components: [row] });
 
                 return int.reply({ content: `Your ticket is open <@${int.member.id}> <#${channel.id}> ✅`, ephemeral: true });
             } else {
